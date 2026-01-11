1ce792f5e925fafddbcf49529bfa668f6003f8a4
SuriTian <suritian9@gmail.com>
2026-01-11 12:33:51 -0500

    Added camera movements

---
+++ Assets/Scripts/MapTransition.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class MapTransition : MonoBehaviour
+{
+    [SerializeField] PolygonCollider2D mapBoundary;
+    CinemachineConfiner confiner;
+
+    private void Awake()
+    {
+        confiner = FindObjectOfType<CinemachineConfiner>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            confiner.m_BoundingShape2D = mapBoundary;
+        }      
+    }
+}
